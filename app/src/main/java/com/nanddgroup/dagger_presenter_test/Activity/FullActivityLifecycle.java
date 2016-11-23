dda854c013b2df1ae906a94d953fab45e604eec0
Nikita Romanenko <nikita241296@gmail.com>
2016-11-23 20:43:26 +0200

    picasso cache test

--- app/src/main/java/com/nanddgroup/dagger_presenter_test/Activity/FullActivityLifecycle.java
+++ app/src/main/java/com/nanddgroup/dagger_presenter_test/Activity/FullActivityLifecycle.java
@@ -3,24 +3,34 @@ package com.nanddgroup.dagger_presenter_test.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.nanddgroup.dagger_presenter_test.R;
+import com.squareup.picasso.Picasso;
 
 /**
  * Show lifecycle with restoring & saving data in rotate time (Instance)
  */
 public class FullActivityLifecycle extends AppCompatActivity {
-    TextView tvLifecycle;
+    ImageView ivLifecycle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        tvLifecycle = (TextView) findViewById(R.id.tvLifecycle);
+        ivLifecycle = (ImageView) findViewById(R.id.ivLifecycle);
         if (savedInstanceState != null)
         Log.wtf("state : ", savedInstanceState.getString("S") + " from onCreate");
         Log.wtf("state : ", "onCreate");
+
+//        Picasso.Builder builder = new Picasso.Builder(this);
+//        builder.downloader(new OkHttpDownloader(this,Integer.MAX_VALUE));
+//        Picasso built = builder.build();
+//        built.setIndicatorsEnabled(true);
+//        built.setLoggingEnabled(true);
+//        Picasso.setSingletonInstance(built);
+        Picasso.with(this).load("https://www.drupal.org/files/images/nodereference_url.png")
+                .into(ivLifecycle);
     }
 
     @Override
